7e081e79bc6443896bf7f993e8297f6d87483d55
Rene Jr <84996527+renecfjunior@users.noreply.github.com>
2023-02-05 18:22:07 -0300

    Add files via upload
    new version

--- src/calculadoraprobabilidades/FXMLDocumentController.java
+++ src/calculadoraprobabilidades/FXMLDocumentController.java
@@ -1,74 +1,58 @@
-
-package calculadoraprobabilidades;
-
-import java.net.URL;
-import java.text.DecimalFormat;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
-
-
-
-public class FXMLDocumentController implements Initializable {
-    
-    @FXML
-    private Label lblMessage;
-
-    @FXML
-    private TextField inptC;
-
-    @FXML
-    private TextField inptT;
-
-    @FXML
-    private Text lblResult;
-
-    @FXML
-    private Button btnCalc;
-    
-    public Double chances;
-    
-    public Double total;
-    
-    public Double result;
-
-    @FXML
-    void onClick(ActionEvent event) {
-     
-        try{
-     // Obtendo o valor em String e convertendo para Double com o parse Double.
-        chances = Double.parseDouble(inptC.getText());
-        
-        total = Double.parseDouble(inptT.getText());
-        
-        result = chances / total * 100;
-        
-        //Formatando o resultado para deixar com apenas duas casas decimais.
-        String formatResult = new DecimalFormat("#.00").format(result);
-        
-        //Apresentando o resultado já formatado na tela.
-        lblResult.setText(formatResult + "%");
-        
-        }
-        // Exception para valores que não sejam números.
-        catch(NumberFormatException e){
-            lblResult.setText("Número Inválido!");
-        }
-        // Exception para tratar algum erro genérico que possa apresentar.
-        catch(Exception e){
-            lblResult.setText("Error");
-        }
-        
-    }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
-}
+package calculadoraprobabilidades; 
+  
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat; 
+import javafx.event.ActionEvent; 
+import javafx.fxml.FXML; 
+import javafx.scene.control.Button; 
+import javafx.scene.control.Label; 
+import javafx.scene.control.TextField; 
+import javafx.scene.text.Text; 
+  
+public class FXMLDocumentController { 
+  
+   @FXML 
+   private Label lblMessage; 
+  
+   @FXML 
+   private TextField inptC; 
+  
+   @FXML 
+   private TextField inptT; 
+  
+   @FXML 
+   private Text lblResult; 
+  
+   @FXML 
+   private Button btnCalc; 
+  
+   @FXML 
+   void onClick(ActionEvent event) { 
+  
+     try { 
+       BigDecimal chances = new BigDecimal(inptC.getText());
+       BigDecimal total = new BigDecimal(inptT.getText());
+  
+       // Verifica se o valor total ou chances são menores que zero.
+       if (total.compareTo(BigDecimal.ZERO) <= 0 || chances.compareTo(BigDecimal.ZERO) <= 0) {
+         lblResult.setText("Entrada inválida");
+         return;
+       }
+  
+       // Cálculo da probabilidade
+       BigDecimal result = chances.divide(total, 10, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
+  
+       //Formatando o resultado para deixar com apenas duas casas decimais. 
+       String formatResult = result.setScale(2, RoundingMode.HALF_UP).toString();
+  
+       //Apresentando o resultado já formatado na tela. 
+       lblResult.setText(formatResult + "%"); 
+  
+     } catch(NumberFormatException e){
+         lblResult.setText("Número Inválido!");
+     } catch(Exception e){
+         lblResult.setText("Error");
+     }
+   } 
+ }
