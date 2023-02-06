fdc5809405e025c6becf4e3df6dd090e70f25578
Rene Jr <84996527+renecfjunior@users.noreply.github.com>
2023-02-06 17:30:18 -0300

    Add files via upload

--- src/calculadoraprobabilidades/FXMLDocumentController.java
+++ src/calculadoraprobabilidades/FXMLDocumentController.java
@@ -2,18 +2,13 @@ package calculadoraprobabilidades;
   
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat; 
 import javafx.event.ActionEvent; 
 import javafx.fxml.FXML; 
 import javafx.scene.control.Button; 
-import javafx.scene.control.Label; 
 import javafx.scene.control.TextField; 
 import javafx.scene.text.Text; 
   
 public class FXMLDocumentController { 
-  
-   @FXML 
-   private Label lblMessage; 
   
    @FXML 
    private TextField inptC; 
@@ -39,20 +34,51 @@ public class FXMLDocumentController {
          lblResult.setText("Entrada inválida");
          return;
        }
-  
-       // Cálculo da probabilidade
+       
+
+              // Cálculo da probabilidade
        BigDecimal result = chances.divide(total, 10, RoundingMode.HALF_UP).multiply(new BigDecimal(100));
   
        //Formatando o resultado para deixar com apenas duas casas decimais. 
-       String formatResult = result.setScale(2, RoundingMode.HALF_UP).toString();
+       BigDecimal formatResult = result.setScale(2, RoundingMode.HALF_UP);
+       if (formatResult.scale() > 2) {
+           throw new Exception("Resultado com mais de duas casas decimais");
+       }
   
        //Apresentando o resultado já formatado na tela. 
        lblResult.setText(formatResult + "%"); 
+
+
+       
+       // Cursor volta para o primeiro input após o cálculo.
+       inptC.requestFocus();
   
-     } catch(NumberFormatException e){
+       
+       
+      } catch(NumberFormatException e){
          lblResult.setText("Número Inválido!");
      } catch(Exception e){
-         lblResult.setText("Error");
+       if (e.getMessage().equals("Resultado com mais de duas casas decimais")) {
+           lblResult.setText("Resultado com mais de duas casas decimais");
+       } else {
+           lblResult.setText("Error");
+       }
      }
    } 
- }
+   //Função para alterar e calcular ao apertar a tecla Enter.
+   @FXML
+   private void handleEnterPressed(ActionEvent event) {
+      if (event.getSource() == inptC) {
+         inptT.requestFocus();
+      } else if (event.getSource() == inptT) {
+         onClick(event);
+      }
+   
+   }
+   
+   @FXML
+   public void initialize() {
+      inptC.setOnAction(this::handleEnterPressed);
+      inptT.setOnAction(this::handleEnterPressed);
+   }
+}
